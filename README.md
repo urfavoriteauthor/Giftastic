0e8efbba91a7641b1d8aab959db30dc57c44d9ec
URxFAVxAUTHOR <nygel.porterfield@gmail.com>
2019-05-12 23:56:38 -0400

    final

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Giftastic
+
+<!-- Remaining necessary code -->
+
+<!-- So the idea was for me to be able to add 10 gifs by cycling through the array of selected items which would've been plugged in to the search field in the query URL. I was having Cors issues with the giphy API. Essentially what I was going to do was generate a new button item with each call from the API that would allow you to generate images of the selected search query. The for loop would cycle through the myFaves array for the length of the array creating a new button each time. Once it reached the length it would kick out of the for loop.  All of the Gifs would've populated to the newGifs div as an appendChild-->
--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,46 +1,61 @@
-$(document).ready(function(){
-    
-    var giphyURL= $.get("https://cors-anywhere.herokuapp.comhttp://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=QvUBFBSSB6pUED03q42N1WDVzJh7CgEB" + myFaves + apiKey);
+$(document).ready(function () {
+
+    var giphyURL = $.get("https://cors-anywhere.herokuapp.comhttp://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=QvUBFBSSB6pUED03q42N1WDVzJh7CgEB" + myFaves + apiKey);
     var apiKey = "QvUBFBSSB6pUED03q42N1WDVzJh7CgEB";
     var myFaves = ["anime", "comic Books", "Football", "MMA", "Rick and Morty", "Video Games", "Bowling", "Eating"];
     var gifButton = $("#gifButton");
-    
 
-    gifButton.on("click", function(){
+
+    var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=QvUBFBSSB6pUED03q42N1WDVzJh7CgEB&limit=10");
+    xhr.done(function (data) {
+        console.log("success got data", data);
+    });
+
+    var gif = data
+
+
+    var createPic = $("<img>");
+    var gifDiv = $("<div>");
+    createPic.attr("src", gif[1].images.url);
+    gifDiv.append(createPic);
+
+    // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
+    $("#newgifs").prepend(gifDiv);
+    gifButton.on("click", function () {
 
         $.ajax({
-            url: giphyURL,
-            method: "GET"
-        })
-        // After the data comes back from the API
-        .then(function(response) {
-            var results = response.data;
-            
-            for(i = 0; i<myFaves.length; i++){
-                var generateButton = $("<button>")
-                $("#newGifs").append(generateButton);
-         generateButton.text(myFaves[i]);
-         
-         if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
-             
-          var gifDiv = $("<div>");
-          
-          var rating = results[i].rating;
-          
-          var p = $("<p>").text("Rating: " + rating);
-          
-          var createPic = $("<img>");
-          
-          createPic.attr("src", myFaves[i].images.fixed_height.url);
-          
-          // Appending the paragraph and personImage we created to the "gifDiv" div we created
-          gifDiv.append(p);
-          gifDiv.append(personImage);
-          
-          // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
-          $("#newgifs").prepend(gifDiv);
-        }
-    }
-})
-})
+                url: giphyURL,
+                method: "GET"
+            })
+            // After the data comes back from the API
+            .then(function (response) {
+                var results = response.data;
+
+                for (i = 0; i < myFaves.length; i++) {
+                    var generateButton = $("<button>")
+                    $("#newGifs").append(generateButton);
+                    generateButton.text(myFaves[i]);
+
+                    if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+
+                        var gifDiv = $("<div>");
+
+                        var rating = results[i].rating;
+
+                        var p = $("<p>").text("Rating: " + rating);
+
+                        var createPic = $("<img>");
+
+                        createPic.attr("src", myFaves[i].images.fixed_height.url);
+
+                        // Appending the paragraph and personImage we created to the "gifDiv" div we created
+                        gifDiv.append(p);
+                        gifDiv.append(personImage);
+
+                        // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
+                        $("#newgifs").prepend(gifDiv);
+                    }
+                }
+            })
+    })
 });
